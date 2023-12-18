d7438e79d5ea71f03988c000dadaa62e1920ea29
Abdi Bacha <110161626+Abdi-B@users.noreply.github.com>
2023-12-18 14:39:26 +0300

    Update contact.ts

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,8 +2,8 @@ import nodemailer from "nodemailer";
 import { google } from "googleapis";
 import createMail from "../../utils/createMail";
 
-const redirectUri = "https://wadafmining5.vercel.app/";
-// const redirectUri = "https://developers.google.com/oauthplayground";
+// const redirectUri = "https://wadafmining5.vercel.app/";
+const redirectUri = "https://developers.google.com/oauthplayground";
 // const redirectUri = "http://localhost:3000/#Contact";
 const appEmailAddress = "abdibacha67@gmail.com";
 const googleRefreshToken =
