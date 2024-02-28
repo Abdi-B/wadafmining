a1c756749f97ca0a63e02e06b917cf1d9079d5f8
Abdi Bacha <110161626+Abdi-B@users.noreply.github.com>
2024-02-28 14:17:45 +0300

    Update contact

--- pages/api/contact 111 copy.ts
+++ pages/api/contact 111 copy.ts
@@ -4,8 +4,8 @@ import createMail from "../../utils/createMail";
 
 const redirectUri = "https://wadafmining2.vercel.app";
 const appEmailAddress = "abdibacha67@gmail.com";
-const googleRefreshToken =
-  "1//04gqz_k__W3HMCgYIARAAGAQSNwF-L9IrFu0mg8-zlKXGur4NpwRoDAlyeq-xGPXFvHe4egcMePsej1IxYDWLBKJdmCTYZcvdX2I";
+// const googleRefreshToken =
+  // "1//04gqz_k__W3HMCgYIARAAGAQSNwF-L9IrFu0mg8-zlKXGur4NpwRoDAlyeq-xGPXFvHe4egcMePsej1IxYDWLBKJdmCTYZcvdX2I";
 
 const googleClientIdMail =
   "527486429491-8hve3j7cgtsd9hfsegfi3vq9ivlrh2kq.apps.googleusercontent.com";
